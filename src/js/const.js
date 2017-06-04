f51958cd72d676eec0546ca769edbd89c21d81cf
Rebecca Hill <r3b3ccahill@gmail.com>
2017-06-04 17:02:28 +0200

    Add filters to panel

---
+++ src/js/const.js
@@ -0,0 +1,13 @@
+export const FILTERS = {
+  WORKSHOP: 'Workshop',
+  MACHINE_BUILDER: 'Machine Builder',
+  SHOP: 'Shop',
+  BUY_PLASTIC: 'Buy Plastic',
+  SELL_PLASTIC: 'Sell Plastic',
+  COLLECT: 'Collect'
+}
+
+export const STATUS = {
+    OPEN: 'Open for visits',
+    CLOSED: 'Closed for visits'
+}
