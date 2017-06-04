3eca1a9192707ce499182d88498c5a79c60b42e3
Ben Naylor <ben.naylor@beethree.nl>
2017-06-05 00:56:18 +0200

    needed babel-polyfill too

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: [ 'babel-polyfill', path.join(__dirname, 'src', 'index.js') ],
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
