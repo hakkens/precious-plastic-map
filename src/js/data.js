2154ecb162b7e366dd5c33984f1d0f7cd34c9520
Ben Naylor <ben.naylor@beethree.nl>
2017-06-04 11:18:28 +0200

    base

---
+++ src/js/data.js
@@ -0,0 +1,17 @@
+var marker_data = {
+    lat: 52.3707599,
+    lng: 4.889869200000021,
+    name: "BOPE",
+    address: “”,
+    email: “”,
+    description: "Lorem ipsum Elit in nisi ut ut aute nisi eiusmod ad velit elit culpa pariatur enim dolor officia consectetur officia eu sint commodo deserunt culpa minim adipisicing laborum.",
+    filters: [ //ENUM
+        "Workshop”,
+        "Machine Builder”,
+        "Shop"
+    ],
+    status: "Open for visit",  //ENUM
+    website: "http://www.bope.th",
+    hashtags: ["Shredder", "Injection", "Extrusion", "Compression"]
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app.js'),
+  devtool: "#inline-source-map",
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Precious Plastic',
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+};
