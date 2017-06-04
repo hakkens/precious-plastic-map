0debe92ee58a59acbbbe009a6f8958300d16e60d
Rebecca Hill <r3b3ccahill@gmail.com>
2017-06-04 15:03:35 +0200

    Add the panel and it's styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 export default class App {
 
   constructor({ data, map }) {
-    //on map, call render
-    //on data, call getLocations
-    //  => on map, call set data
+    // on map, call render
+    // on data, call getLocations
+    // => on map, call set data
   }
 
 }
