faa5947c9274916fc8c8fa04250669cc21c47c84
Rebecca Hill <r3b3ccahill@gmail.com>
2017-06-05 00:21:26 +0200

    Refactor

--- src/js/app.js
+++ src/js/app.js
@@ -7,28 +7,28 @@ export default class App {
   constructor({ data, map }) {
     this.data = data
     this.map = map
+    this.activeFilters = []
+    this.locations = []
   }
 
   async initApp() {
-    const locations = await this.data.getLocations()
-    this.map.setData(locations)
+    this.locations = await this.data.getLocations()
+    this.map.setData(this.locations)
 
-    const filterEls = this.createFilters(locations)
-    this.activateFilters(filterEls, locations)
+    const filters = buildFilters(this.locations)
+    this.createFilterElements(filters)
+
+    this.activeFilters = filters.map(filter => filter.key)
   }
 
-  createFilters(locations) {
-    const filterNames = _
-      .chain(locations)
-      .map('filters')
-      .flatten()
-      .uniq()
-      .map(name => ({ key: name, value: FILTERS[name] }))
-      .value()
-
+  createFilterElements(filters) {
     const container = getElement('filters')
-    return filterNames.map(filter => {
-      const labelEl = createElement({ tag: 'label', cls: 'panel__checkbox-item' })
+
+    filters.forEach(filter => {
+      const labelEl = createElement({
+        tag: 'label',
+        cls: 'panel__checkbox-item'
+      })
       const inputEl = createElement({
         tag: 'input',
         cls: 'panel__checkbox',
@@ -39,29 +39,32 @@ export default class App {
       })
       const textEl = document.createTextNode(filter.value)
 
+      inputEl.addEventListener('click', () => this.toggleFilter(filter.key))
       labelEl.appendChild(inputEl)
       labelEl.appendChild(textEl)
       container.appendChild(labelEl)
-
-      return inputEl
     })
   }
 
-  activateFilters(filterEls, locations) {
-    filterEls.forEach(el => {
-      el.addEventListener('click', () => {
-        const selectedFilters = _
-          .chain(filterEls)
-          .filter(el => el.checked)
-          .map('value')
-          .value()
+  toggleFilter(filterKey) {
+    this.activeFilters = _.xor(this.activeFilters, [filterKey])
 
-        const filteredLocations = locations.filter(location => {
-          return !_.isEmpty(_.intersection(location.filters, selectedFilters))
-        })
-
-        this.map.setData(filteredLocations)
-      })
+    const filteredLocations = this.locations.filter(location => {
+      return !_.isEmpty(_.intersection(location.filters, this.activeFilters))
     })
+    this.map.setData(filteredLocations)
   }
+}
+
+function buildFilters(locations) {
+  return _
+    .chain(locations)
+    .map('filters')
+    .flatten()
+    .uniq()
+    .map(name => ({
+      key: name,
+      value: FILTERS[name]
+    }))
+    .value()
 }
