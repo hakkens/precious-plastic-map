d00dac44827ddfe9c123be615b4d115c173d8401
Ben Naylor <ben.naylor@beethree.nl>
2017-06-05 20:45:02 +0200

    use util function instead

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,7 @@ export default class App {
     const filters = buildFilters(this.locationData)
     this.createFilterElements(filters)
     this.activeFilters = filters.map(filter => filter.key)
-    document.getElementById('add-pin').addEventListener('click', () => this.map.addPin())
+    getElement('add-pin').addEventListener('click', () => this.map.addPin())
     this.setData()
   }
 
