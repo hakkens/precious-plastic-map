92b3cc16997e201e1403f8e595449af29acc1c1b
Ben Naylor <ben.naylor@beethree.nl>
2017-06-10 12:44:06 +0200

    use new endpoint for data + map to existing structure

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,29 @@
+import { FILTERS, HASHTAGS } from './const'
+
 export default class Data {
 
   getLocations() {
-    return fetch('https://raw.githubusercontent.com/nzchicken/precious-plastic-map/data/data.json')
+    const endpoint = process.env.WP_URL + '/wp-json/pp_pins/v1/pins'
+    return fetch(endpoint)
       .then(response => response.json())
+      .then(response => response.map(mapValue))
   }
+}
+
+function mapValue(value) {
+  const filterKeys = Object.keys(FILTERS)
+  const hashtagKeys = Object.keys(HASHTAGS)
+
+  const { lat, long, name, services, tags, status, description, url } = value
+  const newObj = {
+    lat: parseFloat(lat),
+    lng: parseFloat(long),
+    name,
+    status,
+    description,
+    website: url,
+    filters: services.map(index => filterKeys[index - 1]),
+    hashtags: tags.map(index => hashtagKeys[index - 1])
+  }
+  return newObj
 }
