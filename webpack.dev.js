16f39f58e0d4af550fc533a1cebc78ea9255505c
Ben Naylor <ben.naylor@beethree.nl>
2017-06-10 12:42:37 +0200

    add url endpoint to build

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,14 @@ var CommonConfig = require('./webpack.common.js');
 
 var mergedConfig = function(env) {
   return Merge(CommonConfig, {
-    devtool: "#inline-source-maps"
+    devtool: "#inline-source-maps",
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env' : {
+          'WP_URL': JSON.stringify('http://187.217.174.169/ppWP')
+        }
+      })
+    ]
   });
 };
 
