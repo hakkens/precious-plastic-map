8a673ce95b5fc3c6691d6b8742a211e1184c3bb7
Rebecca Hill <r3b3ccahill@gmail.com>
2017-08-01 21:13:40 +0200

    map params

--- src/js/utils.js
+++ src/js/utils.js
@@ -22,3 +22,13 @@ export function addAttribute(el, attrType, attrVal) {
 export function openNewWindow(url) {
   window.open(url, '_blank')
 }
+
+export function getQueryVariable(variable) {
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    const pair = vars[i].split("=");
+    if (pair[0] == variable) { return pair[1]; }
+  }
+  return (false);
+}
