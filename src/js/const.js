3e466f785040ad4ad9ea202a6a64b71254ddee3c
Rebecca Hill <r3b3ccahill@gmail.com>
2017-08-13 21:51:36 +0200

    Adding copy changes and beta testing banner

--- src/js/const.js
+++ src/js/const.js
@@ -1,16 +1,16 @@
 export const FILTERS = {
   WORKSHOP: 'Workspaces',
-  MACHINE: 'Machine Builders',
-  STARTED: 'Wants to Get Started'
+  MACHINE: 'Machine builders',
+  STARTED: 'Want to get started'
 }
 
 export const FILTERS_SINGULAR = {
   WORKSHOP: 'Workspace',
-  MACHINE: 'Machine Builder',
-  STARTED: 'Want to Get Started'
+  MACHINE: 'Machine builder',
+  STARTED: 'Want to get started'
 }
 
 export const STATUS = {
   OPEN: 'Open for visits',
-  CLOSED: 'Closed for visits'
+  CLOSED: 'Not open for visits'
 }
